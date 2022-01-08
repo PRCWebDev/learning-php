6783194ebc44dfadd153807229732e4a42b12248
PRCWebDev <webdevprc@gmail.com>
2022-01-08 09:52:41 +0200

    challenge #3 completed

---
+++ php-challenges/3-multiples.php
@@ -0,0 +1,20 @@
+<?php
+
+// use FOR and WHILE loops to list all the multiples of 6 which are less or equal to 60
+
+// FOR looop
+
+for($i = 6; $i <= 60; $i += 6){
+  echo "$i" . "<br>";
+}
+
+echo "<hr>";
+
+// WHILE loop
+
+$j = 6;
+
+while($j <= 60){
+  echo "$j" . "<br>";
+  $j += 6;
+}
