3be1b2519a2974764705d930481c01ca7aa2a308
PRCWebDev <webdevprc@gmail.com>
2022-01-18 20:15:52 +0200

    challenge #20 completed

---
+++ php-challenges/20-1-all-about-me-form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>All About Me</title>
+</head>
+<body>
+    <form action="20-2-all-about-me-process.php" method="POST">
+        <input type="text" name="name">
+        <input type="number" name="age">
+        <input type="text" name="colour">
+        <input type="submit" name="submit">
+    </form>
+</body>
+</html>
