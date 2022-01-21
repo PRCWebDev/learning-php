1bd9b815e3e1ec26dff6aa71191168562e5f0ba9
PRCWebDev <webdevprc@gmail.com>
2022-01-21 06:48:36 +0200

    challenge #21 completed

---
+++ php-challenges/21-palindrome-checker.php
@@ -0,0 +1,26 @@
+<?php
+
+if(isset($_POST['submit'])){
+	$word = $_POST['word'];
+	$reversedWord = strrev($word);
+	if($word == $reversedWord){
+		echo $word . " is a palindrome";
+	} else {
+		echo $word . " is not a palindrome";
+	}
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Palindromes</title>
+	</head>
+	<body>
+		<form action="" method="post">
+			<input type="text" name="word" required>
+			<input type="submit" name="submit">
+		</form>
+	</body>
+</html>
